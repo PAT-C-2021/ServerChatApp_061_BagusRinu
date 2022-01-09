de032cd68c8557450ea3477baa0ac286b975d5eb
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2022-01-09 14:44:03 +0700

    Add project files.

---
+++ ServerChatApp_061_BagusRinu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_061;
+
+namespace ServerChatApp_061_BagusRinu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready Sir!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
